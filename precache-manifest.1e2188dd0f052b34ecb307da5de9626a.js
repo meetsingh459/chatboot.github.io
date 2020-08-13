af3822a2e600c254813142d7f87061b023b0a314
Manmeet459 <52605382+Manmeet459@users.noreply.github.com>
2020-08-13 11:06:55 +0530

    Updates

---
+++ precache-manifest.1e2188dd0f052b34ecb307da5de9626a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475ff70513dc88974a02de441f1389c4",
+    "url": "/chatboot.github.io/index.html"
+  },
+  {
+    "revision": "7fac93a9fb474a71946c",
+    "url": "/chatboot.github.io/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7eeacd0f4f1373f626ac",
+    "url": "/chatboot.github.io/static/css/main.b2038b64.chunk.css"
+  },
+  {
+    "revision": "7fac93a9fb474a71946c",
+    "url": "/chatboot.github.io/static/js/2.ddfd5419.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chatboot.github.io/static/js/2.ddfd5419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eeacd0f4f1373f626ac",
+    "url": "/chatboot.github.io/static/js/main.1c348e0f.chunk.js"
+  },
+  {
+    "revision": "a4757d3f04e95221ab3c",
+    "url": "/chatboot.github.io/static/js/runtime-main.6132cbb7.js"
+  }
+]);
